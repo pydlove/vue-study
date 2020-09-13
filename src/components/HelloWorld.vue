22ccdcde3c2ea97d39c18ca7f7a4239b1f4faf8e
pydlove <py_world@163.com>
2020-09-13 22:24:51 +0800

    增加前后台交互测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,25 +5,25 @@
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
+      >vue-cli documentation</a
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+                href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+                target="_blank"
+                rel="noopener"
+        >babel</a
         >
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
+                href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+                target="_blank"
+                rel="noopener"
+        >eslint</a
         >
       </li>
     </ul>
@@ -34,17 +34,17 @@
       </li>
       <li>
         <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
+        >Forum</a
         >
       </li>
       <li>
         <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
+        >Community Chat</a
         >
       </li>
       <li>
         <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
+        >Twitter</a
         >
       </li>
       <li>
@@ -55,7 +55,7 @@
     <ul>
       <li>
         <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
+        >vue-router</a
         >
       </li>
       <li>
@@ -63,23 +63,23 @@
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
+                href="https://github.com/vuejs/vue-devtools#vue-devtools"
+                target="_blank"
+                rel="noopener"
+        >vue-devtools</a
         >
       </li>
       <li>
         <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
+        >vue-loader</a
         >
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
+                href="https://github.com/vuejs/awesome-vue"
+                target="_blank"
+                rel="noopener"
+        >awesome-vue</a
         >
       </li>
     </ul>
@@ -87,34 +87,44 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  created() {
-    console.log(process.env)
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        created() {
+            console.log(process.env)
+            this.showExample("")
+        },
+        methods: {
+            async showExample(params) {
+                let data = await this.$aiorequest(this.$aiocUrl.portal_service_v1_test, params) // 调用接口得到返回值处理
+                if (data.code === "200" || data.code === "SUCCESS" ) {
+                    console.log(data.data)
+                    return true
+                }
+            },
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello {
-  margin-top: 60px;
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .hello {
+    margin-top: 60px;
+  }
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
