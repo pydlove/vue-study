87b39817e36546a4c4be4c53ef168d5e792e80a1
pydlove <py_world@163.com>
2020-09-13 22:23:22 +0800

    增加测试请求

--- src/service/apiUrl.js
+++ src/service/apiUrl.js
@@ -2,5 +2,8 @@
  * apiurl list
  */
 export default {
-  example: '/example' // example
+  example: '/example', // example
+
+  // portal 测试请求
+  portal_service_v1_test : "/portal-service/v1/test/",
 }
