f472e5992720a9d239682b138247b839c8155d97
pydlove <py_world@163.com>
2020-09-16 21:00:48 +0800

    修改增加界面，增加邮箱、短信发送

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -14,30 +14,34 @@
                     <el-divider class="wdi-350" content-position="center">使用手机号/邮箱登录</el-divider>
                 </div>
                 <div class="login-form">
-                    <el-input class="mb-10" v-model="form.phoneOrMailbox" placeholder="请输入手机号码或邮箱"></el-input>
-                    <el-input class="mb-10" v-model="form.password" placeholder="密码" show-password="true"></el-input>
-                    <slide-verify v-show="showSlide"
-                            ref="slideblock"
-                            @again="onAgain"
-                            @fulfilled="onFulfilled"
-                            @success="onSuccess"
-                            @fail="onFail"
-                            @refresh="onRefresh"
-                            :accuracy="accuracy"
-                            :slider-text="text"
-                            :w="350"
-                    ></slide-verify>
-                    <div>{{msg}}</div>
-                    <el-button class="mti-10 wd-350" type="primary">登 录</el-button>
+
+                    <el-input :class="errorMsg.length > 0 ? 'bor-red mb-10 wd-350' : 'mb-10 wd-350'" v-model="form.mailOrPhone"
+                              placeholder="请输入手机号码或者邮箱地址"
+                              @blur="checkFormat"
+                    ></el-input>
+                    <div class="errormsg">{{errorMsg}}</div>
+
+                    <el-input class="mb-10" v-model="form.password"
+                              placeholder="请输入密码" show-password="true"
+                              @blur="checkPassword"
+                    ></el-input>
+                    <div class="pwdErrormsg">{{pwdErrorMsg}}</div>
+
+                    <div class="verification-main mb-5">
+                        <el-input class="verification-in" placeholder="请输入验证码" v-model="form.verificationCode" autocomplete="off"></el-input>
+                        <img id="imgIdentifyingCode" src="/cpcm-service/v1/login/captcha.jpg"
+                             class="verification" alt="点击更换" title="点击更换" @click="getIdentifyingCode()" />
+                    </div>
+
+                    <el-button class="mti-10 wd-350" type="primary" @click="submit">登 录</el-button>
                 </div>
                 <div class="login-ba mt-10 wd-350">
-                    <a class="fl">忘记密码»</a>
+                    <a class="fl" @click="toForgetPage">忘记密码»</a>
                     <a class="fr" @click="toRegisterPage">点击注册»</a>
                     <a class="fr">还没有梧桐圈帐号？</a>
                 </div>
             </div>
         </el-dialog>
-
     </div>
 </template>
 
@@ -45,12 +49,63 @@
 
     export default {
         name: "login",
+        mounted() {
+
+        },
         methods: {
             close() {
                 this.dialogVisible = false;
             },
             open() {
                 this.dialogVisible = true;
+            },
+
+            /**
+             * 图片验证码
+             */
+            getIdentifyingCode: function () {
+                let identifyCodeSrc = this.$aiocUrl.cpcm_service_v1_login_captcha + "?" + Math.random();
+                let objs = document.getElementById("imgIdentifyingCode");
+                objs.src = identifyCodeSrc;
+            },
+
+            /**
+             * 检测格式
+             */
+            checkFormat() {
+                var mailReg = new RegExp("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$");
+                var phoneReg = new RegExp("^[1][3-8]\\d{9}$|^([6|9])\\d{7}$|^[0][9]\\d{8}$|^[6]([8|6])\\d{5}$");
+                if(!this.form.mailOrPhone) {
+                    this.errorMsg = "请输入手机号或者邮箱地址";
+                    return false;
+                }
+                else if(!(mailReg.test(this.form.mailOrPhone)) && !(phoneReg.test(this.form.mailOrPhone))){
+                    this.errorMsg = "请输入正确的手机号码（支持澳门台湾）或邮箱";
+                    return false;
+                }
+                else if((mailReg.test(this.form.mailOrPhone))){
+                    this.errorMsg = "";
+                    this.loginType = "mail";
+                    return true;
+                }
+                else if((phoneReg.test(this.form.mailOrPhone))){
+                    this.errorMsg = "";
+                    this.loginType = "phone";
+                    return true;
+                }
+            },
+
+            /**
+             * 密码校验
+             */
+            checkPassword() {
+                if(!this.form.password) {
+                    this.pwdErrorMsg = "请输入密码";
+                    return false;
+                } else {
+                    this.pwdErrorMsg = "";
+                    return true;
+                }
             },
 
             /**
@@ -61,29 +116,40 @@
                 this.$emit("toRegisterPage");
             },
 
-            /**
-             * 滑块相关方法
+             /**
+             * 跳转到忘记密码界面
              */
-            onSuccess(){
-                console.log('验证通过');
-                this.msg = 'login success'
+             toForgetPage() {
+                this.close();
+                this.$emit("toForgetPage");
             },
-            onFail(){
-                console.log('验证不通过');
-                this.msg = ''
-            },
-            onRefresh(){
-                console.log('点击了刷新小图标');
-                this.msg = ''
-            },
-            onFulfilled() {
-                console.log('刷新成功啦！');
-            },
-            onAgain() {
-                console.log('检测到非人为操作的哦！');
-                this.msg = 'try again';
-                // 刷新
-                this.$refs.slideblock.reset();
+
+            /**
+             * 登陆
+             */
+            async submit() {
+                const checkFormatResult = this.checkFormat();
+                if(!checkFormatResult) {
+                    return;
+                }
+                const checkPasswordResult = this.checkPassword();
+                if(!checkPasswordResult) {
+                    return;
+                }
+                let params = new FormData()
+                params.append("principal", this.form.mailOrPhone);
+                params.append("password", this.$md5(this.form.password));
+                params.append("verificationCode",  this.form.verificationCode);
+                params.append("type", this.loginType);
+                params.append("rememberMe", this.form.rememberMe);
+                let data = await this.$aiorequest(this.$aiocUrl.cpcm_service_v1_login, params, "POST");
+                if (data.code === 200) {
+                    this.close();
+                    console.log(data.data)
+                    return true;
+                } else {
+                    this.getIdentifyingCode();
+                }
             },
         },
         data() {
@@ -94,16 +160,20 @@
                 /* logo图片地址 */
                 logo: require('@/assets/img/logo/logo-2.png'),
 
-                /* 精确度小，可允许的误差范围小；为1时，则表示滑块要与凹槽完全重叠，才能验证成功。默认值为5 */
-                accuracy: 1,
-                msg: '',
-                text: '向右滑',
-                /* slide的显示和隐藏 */
-                showSlide: true,
+                /* 手机号码或者邮箱错误提示 */
+                errorMsg: "",
+
+                /* 密码校验错误提示 */
+                pwdErrorMsg: "",
+
+                /* 注册方式 */
+                loginType: "",
 
                 form: {
-                    phoneOrMailbox: "",
-                    password: "",
+                    mailOrPhone: "py_world@163.com",
+                    password: "1q2w3e4r!@",
+                    verificationCode: "",
+                    rememberMe: false,
                 }
             }
         },
@@ -156,6 +226,31 @@
         .login-ba a:nth-of-type(2):hover {
             text-decoration: underline;
         }
+        .verification-main {
+            display: flex;
+        }
+        .verification-in {
+            width: 60%;
+        }
+        .verification {
+            width: 40%;
+            height: 40px;
+            margin-left: 10px;
+            border-radius: 4px;
+        }
+        .errormsg {
+            text-align: left;
+            margin-bottom: 5px;
+            padding-left: 5px;
+            color: red;
+        }
+        .pwdErrormsg {
+            text-align: left;
+            padding-left: 5px;
+            margin-bottom: 5px;
+            color: red;
+        }
+
     }
 
     /*媒体查询（平板）*/
