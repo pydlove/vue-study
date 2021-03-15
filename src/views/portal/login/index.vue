2db8788db0bc557fde818b028528013d512d294e
pydlove <py_world@163.com>
2021-03-15 09:24:57 +0800

    初始化导入

--- src/views/portal/login/index.vue
+++ src/views/portal/login/index.vue
@@ -1,51 +1,65 @@
 <template>
-    <div class="loginc" :style="'background-image:url(' + mainBackground + '); height:' + clientHeight + 'px; width:' + clientWidth + 'px;'">
+    <!--eslint-disable-->
+    <div class="loginc"
+         :style="'background-image:url(' + mainBackground + '); height:' + clientHeight + 'px; width:' + clientWidth + 'px;'">
         <div class="lgrc">
-            <el-carousel class="tpc wow bounceInLeft" direction="vertical" ndicator-position="outside" interval="2000" autoplay="true">
-                <el-carousel-item class="tpc" v-for="item in advertisingBackgrounds" :key="item" >
+            <el-carousel class="tpc wow bounceInLeft" direction="vertical" ndicator-position="outside" interval="2000"
+                         autoplay="true">
+                <el-carousel-item class="tpc" v-for="item in advertisingBackgrounds" :key="item">
                     <el-image class="tpc"
-                            :src="item"
-                            :fit="fit"></el-image>
+                              :src="item"
+                              :fit="fit"></el-image>
                 </el-carousel-item>
             </el-carousel>
             <div class="gnc wow bounceInRight">
                 <div class="gnm">
-                    <span class="lotitle">安徽柏林书画研究院网络管理系统</span>
-                    <p class="dl">控制台登录</p>
+                    <div class="xstd">安徽省伯林书画研究院</div>
+                    <div class="xstd mb-20"> <i class="xuesheng mr-10"></i>学生端</div>
 
-                    <el-form class="lform" label-width="80px" :model="loginForm">
-                        <div class="sr">
-                            <span class="phone-icon"></span>
-                            <el-input class="srin" v-model="loginForm.name" placeholder="请输入账号"></el-input>
-                        </div>
-
-                        <div class="sr">
-                            <span class="code-icon"></span>
-                            <el-input class="srin" v-model="loginForm.password" placeholder="请输入登录密码"></el-input>
-                        </div>
-
-                        <div class="sr">
-                            <span class="code-icon"></span>
-                            <el-input class="srin vcf" placeholder="请输入验证码" v-model="loginForm.verificationCode" autocomplete="off"></el-input>
-                            <!--<img id="imgIdentifyingCode" src="/cpcm-service/v1/login/captcha.jpg"-->
-                                 <!--class="vcimg" alt="点击更换" title="点击更换" @click="getIdentifyingCode()" />-->
-                            <div class="testimg">
-                                <span>5</span>
-                                <span>9</span>
-                                <span>a</span>
-                                <span>h</span>
+                    <el-form class="login-form" ref="form" :rules="rules" label-width="0px" :model="loginForm">
+                        <el-form-item label="" prop="phone" required>
+                            <div class="rsr">
+                                <span class="phone-icon"></span>
+                                <el-input class="srin" v-model="loginForm.phone" placeholder="请输入手机号"></el-input>
                             </div>
-                        </div>
+                        </el-form-item>
+
+                        <el-form-item label="" prop="password" required>
+                            <div class="rsr">
+                                <span class="phone-icon"></span>
+                                <el-input class="srin" v-model="loginForm.password" placeholder="请输入登录密码" show-password></el-input>
+                            </div>
+                        </el-form-item>
+
+                        <el-form-item label="" prop="verificationCode" required>
+                            <div class="sr">
+                                <span class="code-icon"></span>
+                                <el-input class="srin vcf" placeholder="请输入验证码" v-model="loginForm.verificationCode"
+                                          autocomplete="off"></el-input>
+                                <img id="imgIdentifyingCode" src="/blsh-service/v1/login/captcha.jpg"
+                                     class="vcimg" alt="点击更换" title="点击更换" @click="getIdentifyingCode()"/>
+                            </div>
+                        </el-form-item>
 
                         <el-button class="dl-btn" type="primary" @click="login">立即登录</el-button>
                     </el-form>
+
+                    <div class="lbt-tip bt mt-10">
+                        <!--<a class="fl" @click="toForget">忘记密码»</a>-->
+                        <a class="fr" @click="toRegister">点击注册»</a>
+                        <a class="fr">还没有帐号？</a>
+                    </div>
                 </div>
             </div>
         </div>
-        <Footer class="fter"></Footer>
+
+        <!--<div class="zc">-->
+        <!--目前不支持IE、360浏览器，建议使用谷歌、火狐、Microsoft Edge等浏览器访问-->
+        <!--</div>-->
+        <!--<Footer class="fter"></Footer>-->
     </div>
 </template>
-
+<!--eslint-disable-->
 <script>
     import Footer from "@/components/Footer"
 
@@ -53,43 +67,131 @@
         name: "sindex",
         components: {Footer},
         mounted() {
-            this.clientWidth = document.body.clientWidth;
-            this.clientHeight = document.body.clientHeight;
             new this.$wow.WOW({
                 live: false
             }).init();
+
+            this.$nextTick(() => {
+                this.$refs["form"].clearValidate();
+            });
         },
         methods: {
+            clearForm() {
+                this.loginForm = {
+                    name: "",
+                    password: "",
+                    verificationCode: "",
+                    rememberMe: false,
+                };
+            },
+
             login() {
-                this.$router.push({name: "portal"});
+                this.$refs['form'].validate((valid) => {
+                    if (valid) {
+                        this.submitRequest();
+                    } else {
+                        return false;
+                    }
+                });
+            },
+
+            async submitRequest() {
+                let params = new FormData()
+                params.append("phone", this.loginForm.phone.trim());
+                params.append("password", this.$md5(this.loginForm.password));
+                params.append("verificationCode", this.loginForm.verificationCode.trim());
+                params.append("rememberMe", this.loginForm.rememberMe);
+                let data = await this.$aiorequest(this.$aiocUrl.blsh_service_v1_login, params, "POST");
+                if (data.code == 200) {
+                    this.initStorage();
+                    console.log(data.data)
+                    this.$utils.setStorage("staioctoken", data.data);
+                    this.user = data.data;
+                    // == 2 去首次登录
+                    // else 去首页
+                    if(this.user.status == "2") {
+                        this.$router.push({
+                            name: "firstLogin"
+                        });
+                    } else if(this.user.status == "0") {
+                        this.$router.push({
+                            name: "main"
+                        });
+                    } else if(this.user.status == "1") {
+                        this.$notify.error({
+                            title: '提示',
+                            message: '您的账号目前处于静默状态，不能正常访问登录，请联系客服'
+                        });
+                        this.$router.push({
+                            name: "silent"
+                        });
+                    }
+                    return true;
+                } else {
+                    this.getIdentifyingCode();
+                }
+            },
+
+            initStorage() {
+                this.$utils.removeStorage("bluserForm");
+            },
+
+            toForget() {
+                this.$router.push({name: "forgetPwd"});
+            },
+
+            toRegister() {
+                this.$router.push({name: "register"});
             },
 
             /**
              * 图片验证码
              */
             getIdentifyingCode: function () {
-                let identifyCodeSrc = this.$aiocUrl.cpcm_service_v1_login_captcha + "?" + Math.random();
+                let identifyCodeSrc = this.$aiocUrl.blsh_service_v1_login_captcha + "?" + Math.random();
                 let objs = document.getElementById("imgIdentifyingCode");
                 objs.src = identifyCodeSrc;
             },
         },
         data() {
             return {
+                user: "",
                 // 背景图
-                mainBackground: require("../../../assets/img/logo/shuhua1.jpg"),
+                title: require("../../assets/img/logo/17.png"),
+                mainBackground: require("../../assets/img/logo/shuhua1.jpg"),
                 advertisingBackgrounds: [
-                    require("../../../assets/img/huangshan.jpg"),
-                    require("../../../assets/img/daozi.jpg"),
-                    require("../../../assets/img/reqiqiu.jpg"),
-                    require("../../../assets/img/badalag.jpg"),
-                    require("../../../assets/img/shuig.jpg"),
+                    require("../../assets/img/em/ls.png"),
                 ],
-                clientWidth: 1580,
-                clientHeight: 980,
+                clientWidth: document.body.clientWidth,
+                clientHeight: document.body.clientHeight,
                 loginForm: {
-                    name: "",
+                    // phone: "13588404613",
+                    // password: "1q2w3e4R!Q",
+                    // verificationCode: "xxxx",
+                    phone: "",
                     password: "",
                     verificationCode: "",
+                    rememberMe: false,
+                },
+                rules: {
+                    phone: [
+                        {type: 'string', required: true, message: '请输入手机号', trigger: ['change', 'blur']},
+                        {
+                            validator: function (rule, value, callback) {
+                                if (/^1[34578]\d{9}$/.test(value) == false) {
+                                    callback(new Error("请输入正确的手机号"));
+                                } else {
+                                    callback();
+                                }
+                            }, trigger: ['change', 'blur']
+                        }
+                    ],
+                    password: [
+                        {type: 'string', required: true, message: '请输入密码', trigger: ['change', 'blur']},
+                    ],
+                    verificationCode: [
+                        {type: 'string', required: true, message: '请输入验证码', trigger: ['change', 'blur']},
+                    ],
                 },
             }
         },
@@ -97,134 +199,145 @@
 </script>
 
 <style scoped>
-        .testimg {
-            width: 85px;
-            height: 20px;
-            background: #ffac56;
-            margin-left: 13px;
-            line-height: 20px;
-            font-size: 12px;
-            letter-spacing: 2px;
-        }
-        .testimg > span:nth-of-type(1) {
-            color: #aec2b3;
-        }
-        .testimg > span:nth-of-type(2) {
-            color: #4f77c2;
-        }
-        .testimg > span:nth-of-type(3) {
-            color: #c28122;
-        }
-        .testimg > span:nth-of-type(4) {
-            color: #c20921;
-        }
-        .loginc {
-            background-repeat: no-repeat;
-            background-size: cover;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        .lgrc {
-            min-width: 500px;
-            display: flex;
-            flex-wrap: nowrap;
-        }
-        .tpc {
-            width: 500px;
-            height: 500px;
-            border-top-left-radius: 10px;
-            border-bottom-left-radius: 10px;
-        }
-        .gnc {
-            width: 500px;
-            height: 500px;
-            border-top-right-radius: 10px;
-            border-bottom-right-radius: 10px;
-            background: #ffffff;
-            display: flex;
-            justify-content: center;
-        }
-        .gnm{
-            width: 320px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding-top: 50px;
-            position: relative;
-        }
-        .dl {
+    .xstd {
+        font-size: 26px;
+        color: #000000;
+        margin-top: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 800;
+        font-family: 华文行楷 !important;
+    }
+    .loginc {
+        background-repeat: no-repeat;
+        background-size: cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .lgrc {
+        min-width: 500px;
+        display: flex;
+        flex-wrap: nowrap;
+    }
+    .tpc {
+        width: 500px;
+        height: 500px;
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+    .gnc {
+        width: 500px;
+        height: 500px;
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+        background: #ffffff;
+        display: flex;
+        justify-content: center;
+    }
+    .gnm {
+        width: 320px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 20px;
+        position: relative;
+    }
+    .dl {
 
-            font-size: 20px;
-            color: #606266;
-            margin-top: 40px;
-        }
-        .lform {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 40px;
-            width: 100%;
-        }
-        .sr {
-            display: flex;
-            flex-wrap: nowrap;
-            width:100%;
-            border-bottom: 1px solid #DCDFE6;
-            padding-bottom: 20px;
-            margin: 10px 0 10px 0;
-        }
-        .phone-icon {
-            display: block;
-            width: 14px;
-            height: 17px;
-            background-size: 100% 100%;
-            background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTMiIGhlaWdodD0iMTciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgc3Ryb2tlPSIjNEM0QzRDIiBzdHJva2Utd2lkdGg9IjIiIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHJlY3QgeD0iMSIgeT0iMSIgd2lkdGg9IjExIiBoZWlnaHQ9IjE1IiByeD0iMiIvPjxwYXRoIGQ9Ik02IDEzaDEiIHN0cm9rZS1saW5lY2FwPSJzcXVhcmUiLz48L2c+PC9zdmc+");
-        }
-        .code-icon {
-            display: block;
-            height: 17px;
-            width: 17px;
-            background-size: 100% 100%;
-            background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgc3Ryb2tlPSIjNEM0QzRDIiBzdHJva2Utd2lkdGg9IjIiIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHJlY3QgeD0iMSIgeT0iNyIgd2lkdGg9IjE0IiBoZWlnaHQ9IjkiIHJ4PSIyIi8+PHBhdGggZD0iTTMgN2gxMFY1YTQgNCAwIDAwLTQtNEg3YTQgNCAwIDAwLTQgNHYyeiIvPjxwYXRoIGQ9Ik03IDEzaDIiIHN0cm9rZS1saW5lY2FwPSJzcXVhcmUiLz48L2c+PC9zdmc+");
-        }
-        .srin {
-            height: 17px;
-        }
-        .vcf {
-            width: 200px;
-        }
-        .vcimg {
-            width: 90px;
-            height: 25px;
-            margin-left: 10px;
-        }
-        .dl-btn {
-            width: 100%;
-            padding: 15px 20px;
-            margin-top: 20px;
-        }
-        .bt {
-            width: 100%;
-            font-size: 14px;
-            position: absolute;
-            bottom: 40px;
-        }
-        .lbt-tip a:nth-of-type(1) {
-            color: #ec414d;
-        }
-        .lbt-tip a:nth-of-type(2) {
-            color: #ec414d;
-        }
-        .lbt-tip a:nth-of-type(1):hover {
-            text-decoration: underline;
-        }
-        .lbt-tip a:nth-of-type(2):hover {
-            text-decoration: underline;
-        }
+        font-size: 20px;
+        color: #606266;
+        margin-top: 80px;
+    }
+    .sr {
+        display: flex;
+        flex-wrap: nowrap;
+        width: 100%;
+        border-bottom: 1px solid #DCDFE6;
+        padding-bottom: 10px;
+        margin: 10px 0 10px 0;
+    }
+    .phone-icon {
+        display: block;
+        width: 14px;
+        height: 17px;
+        background-size: 100% 100%;
+        background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTMiIGhlaWdodD0iMTciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgc3Ryb2tlPSIjNEM0QzRDIiBzdHJva2Utd2lkdGg9IjIiIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHJlY3QgeD0iMSIgeT0iMSIgd2lkdGg9IjExIiBoZWlnaHQ9IjE1IiByeD0iMiIvPjxwYXRoIGQ9Ik02IDEzaDEiIHN0cm9rZS1saW5lY2FwPSJzcXVhcmUiLz48L2c+PC9zdmc+");
+    }
+    .code-icon {
+        display: block;
+        height: 17px;
+        width: 17px;
+        background-size: 100% 100%;
+        background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgc3Ryb2tlPSIjNEM0QzRDIiBzdHJva2Utd2lkdGg9IjIiIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHJlY3QgeD0iMSIgeT0iNyIgd2lkdGg9IjE0IiBoZWlnaHQ9IjkiIHJ4PSIyIi8+PHBhdGggZD0iTTMgN2gxMFY1YTQgNCAwIDAwLTQtNEg3YTQgNCAwIDAwLTQgNHYyeiIvPjxwYXRoIGQ9Ik03IDEzaDIiIHN0cm9rZS1saW5lY2FwPSJzcXVhcmUiLz48L2c+PC9zdmc+");
+    }
+
+    .srin {
+        height: 17px;
+        display: flex;
+    }
+
+    .vcf {
+        width: 230px;
+    }
+
+    .vcimg {
+        width: 90px;
+        height: 25px;
+        margin-left: 10px;
+    }
+
+    .dl-btn {
+        width: 100%;
+        padding: 15px 20px;
+        margin-top: 20px;
+    }
+
+    .bt {
+        width: 100%;
+        font-size: 14px;
+        position: absolute;
+        bottom: 40px;
+        right: 0px;
+    }
+
+    .lbt-tip a:nth-of-type(1) {
+        color: #ec414d;
+    }
+
+    /*.lbt-tip a:nth-of-type(2) {*/
+    /*color: #ec414d;*/
+    /*}*/
+
+    .lbt-tip a:nth-of-type(1):hover {
+        text-decoration: underline;
+    }
+
+    /*.lbt-tip a:nth-of-type(2):hover {*/
+    /*text-decoration: underline;*/
+    /*}*/
+
+    .rsr {
+        display: flex;
+        flex-wrap: nowrap;
+        width: 360px;
+        border-bottom: 1px solid #DCDFE6;
+        padding-bottom: 15px;
+        margin: 10px 0 10px 0;
+    }
+    .zc {
+        position: absolute;
+        bottom: 60px;
+        right: 10px;
+        color: #000000;
+        font-weight: 600;
+    }
+
     /*媒体查询（电脑）*/
     @media screen and (min-width: 1529px) {
     }
+
     /*媒体查询（平板）*/
     @media screen and (min-width: 769px) and (max-width: 996px) {
 
@@ -233,23 +346,28 @@
     /*媒体查询（手机）*/
     @media screen and (max-width: 768px) {
 
-
     }
 </style>
 
 <style>
+    .srin .el-input__inner {
+        height: 17px;
+        border: 0px;
+        color: #606266;
+        height: 17px;
+        line-height: 17px;
+        outline: 0;
+        padding: 0 10px;
+        margin-left: 10px;
+        width: 100%;
+    }
+
+    .login-form .el-form-item {
+        margin-bottom: 15px;
+    }
+
     /*媒体查询（电脑）*/
     @media screen and (min-width: 1529px) {
-        .srin .el-input__inner {
-            height: 17px;
-            border: 0px;
-            color: #606266;
-            height: 17px;
-            line-height: 17px;
-            outline: 0;
-            padding: 0 10px;
-            margin-left: 10px;
-            width: 100%;
-        }
+
     }
 </style>
