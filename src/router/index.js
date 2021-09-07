68735356e33a4d1a3ef0813f6af94018c6df3354
pydlove <py_world@163.com>
2021-09-07 15:47:20 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -23,84 +23,84 @@ const router = new Router({
             redirect: '/home'
         },
         {
-            meta: {
-                title: this.$t('menu.home'),
-            },
+            // meta: {
+            //     title: this.$t('menu.home'),
+            // },
             path: '/home',
             name: 'home',
             component: (resolve) => require(['../views/home/index.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.newsResources'),
-            },
+            // meta: {
+            //     title: this.$t('menu.newsResources'),
+            // },
             path: '/newsAndResource',
             name: 'newsAndResource',
             component: (resolve) => require(['../views/news/index.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.newsResources'),
-            },
+            // meta: {
+            //     title: this.$t('menu.newsResources'),
+            // },
             path: '/newsDetail',
             name: 'newsDetail',
             component: (resolve) => require(['../views/news/detail.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.observationEquipment'),
-            },
+            // meta: {
+            //     title: this.$t('menu.observationEquipment'),
+            // },
             path: '/observeDevice',
             name: 'observeDevice',
             component: (resolve) => require(['../views/observeDevice/index.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.observationEquipment'),
-            },
+            // meta: {
+            //     title: this.$t('menu.observationEquipment'),
+            // },
             path: '/observeDeviceDetail',
             name: 'observeDeviceDetail',
             component: (resolve) => require(['../views/observeDevice/detail.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.galleryAchievement'),
-            },
+            // meta: {
+            //     title: this.$t('menu.galleryAchievement'),
+            // },
             path: '/pictureAndAchievement',
             name: 'pictureAndAchievement',
             component: (resolve) => require(['../views/pictureAndAchievement/index.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.observationData'),
-            },
+            // meta: {
+            //     title: this.$t('menu.observationData'),
+            // },
 
             path: '/observationData',
             name: 'observationData',
             component: (resolve) => require(['../views/observationData/index.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.observationData'),
-            },
+            // meta: {
+            //     title: this.$t('menu.observationData'),
+            // },
 
             path: '/observationDataDetail',
             name: 'observationDataDetail',
             component: (resolve) => require(['../views/observationData/observationDataDetail.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.observationApplication'),
-            },
+            // meta: {
+            //     title: this.$t('menu.observationApplication'),
+            // },
 
             path: '/application',
             name: 'application',
             component: (resolve) => require(['../views/application/index.vue'], resolve),
         },
         {
-            meta: {
-                title: this.$t('menu.AboutUs'),
-            },
+            // meta: {
+            //     title: this.$t('menu.AboutUs'),
+            // },
 
             path: '/aboutWe',
             name: 'aboutWe',
