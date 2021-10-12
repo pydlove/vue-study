217b011666895a5736ee154b0c6b03357d9f4519
嗨嗨嗨 <aiocloud_shuxing@126.com>
2021-10-12 09:17:25 +0800

    观测数据下载

--- src/views/observationData/download.vue
+++ src/views/observationData/download.vue
@@ -14,20 +14,23 @@
             <el-table
                     border
                     ref="table"
-                    :data="tableData"
+                    :data="multipleSelection"
                     @sort-change="sortChange"
                     @selection-change='onTableSelectChange'
                     @row-click='tableRowClick'
                     :row-style="{height:'20px'}"
                     :cell-style="{padding:'9px 1px'}">
                 <el-table-column fixed="left" type="selection" width="55" align="center"></el-table-column>
-                <el-table-column prop="fitsUrls" label="文件地址" :show-overflow-tooltip="true" align="center"></el-table-column>
-                <el-table-column prop="sizes" label="文件大小" :show-overflow-tooltip="true" align="center" sortable></el-table-column>
+                <el-table-column prop="fitsName" label="文件名称" :show-overflow-tooltip="true" align="center"></el-table-column>
+                <el-table-column prop="fitsUrl" label="文件地址" :show-overflow-tooltip="true" align="center"></el-table-column>
+                <el-table-column prop="csize" label="文件大小" :show-overflow-tooltip="true" align="center" sortable></el-table-column>
+                <el-table-column prop="status" label="状态" :show-overflow-tooltip="true" align="center" sortable></el-table-column>
             </el-table>
 
             <span slot="footer" class="dialog-footer">
               <el-button class="wdi-120" @click="close">取 消</el-button>
               <el-button class="wdi-120 aioc-btn1" type="primary" @click="onSubmit">确 定</el-button>
+              <el-button class="wdi-120 aioc-btn1" type="primary" @click="downloadFits">下 载</el-button>
             </span>
 
         </el-dialog>
@@ -36,17 +39,90 @@
 </template>
 <!--eslint-disable-->
 <script>
+    import JSZip from'jszip'
+    import FileSaver from'file-saver'
+
     export default {
         name: "download",
         mounted() {
+            this.baseUrl = this.$aiocUrl.baseUrl;
+            this.filesPath = this.multipleSelection;
+            console.log(this.filesPath);
         },
         methods: {
             open(){
                 this.dialogVisible = true;
                 console.log(this.multipleSelection);
+                this.filesPath = this.multipleSelection;
+                console.log( this.filesPath);
             },
             close() {
                 this.dialogVisible = false;
+            },
+
+            //通过url 转为blob格式的数据
+            getImgArrayBuffer(url){
+                let _this = this;
+                return new Promise((resolve, reject) => {
+                    //通过请求获取文件blob格式
+                    let xmlhttp = new XMLHttpRequest();
+                    console.log("url是：" + url);
+                    xmlhttp.open("GET", url, true);
+                    xmlhttp.responseType = "blob";
+                    xmlhttp.onload = function () {
+                        if (this.status == 200) {
+                            resolve(this.response);
+                        }else{
+                            reject(this.status);
+                        }
+                    }
+                    xmlhttp.send();
+                });
+            },
+
+            handleDownLoad() {
+                let _this = this;
+                let zip = new JSZip();
+                let obj = {};
+                let promises = [];
+                _this.title = '正在加载压缩文件';
+                // 循环文件数组
+                for (let i = 0; i < this.filesPath.length; i++) {
+                    if(this.filesPath[i].fitsUrls != '') {
+                         const promise= _this.getImgArrayBuffer("/web-service" + this.filesPath[i].fitsUrl).then(res => {
+                        // const promise= _this.getImgArrayBuffer("/web-service/testdata/chase/2021/07/15/sun_2566.fts").then(res => {
+                            // 下载文件, 并存成ArrayBuffer对象(blob)
+                            zip.file(this.filesPath[i].fitsName, res, { binary: true }); // 逐个添加文件
+                            // zip.file("sun_2565.fts", res, { binary: true }); // 逐个添加文件
+                            // 这里截取我是为了打印
+                            obj [this.filesPath[i].fitsName + '.' + res.type.substring(6)] = res;
+                            console.log('``````````````````````````````',obj )
+                        });
+                        promises.push(promise);
+                    }else {
+                        return this.$message.error('空文件不允许下载  !');
+                    }
+                }
+                Promise.all(promises).then(() => {
+                    zip.generateAsync({ type: "blob" }).then(res=> {
+                        _this.title = '正在压缩中';
+                        // 生成二进制流
+                        FileSaver.saveAs(res, 'Fits文件'); // 利用file-saver保存文件,可以改成自己想要的压缩包名称
+                        _this.title = '压缩完成';
+                    });
+                }).catch(res=>{
+                    _this.$message.error('文件压缩失败');
+                });
+                this.dialogVisible = false; // 弹窗关闭
+            },
+
+            downloadFits(){
+                console.log(this.baseUrl);
+               // let a = document.createElement('a');
+               // // a.href = this.baseUrl + "/testdata/chase/2021/07/15/sun_2566.fts";
+               // a.href = "http://114.212.174.143:8088/testdata/chase/2021/07/15/sun_2566.fts";
+               // a.click();
+                this.handleDownLoad();
             },
 
             onSubmit(){
@@ -113,6 +189,8 @@
                     fitsUrls:"",
                     sizes:"",
                 },
+                filesPath:[],
+                baseUrl: "",
 
             }
         },
