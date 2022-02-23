62b45909efb9a0f096b54a5dbf7c9d5dad4df57f
pydlove <py_world@163.com>
2022-02-23 16:38:44 +0800

    初始化

--- src/service/apiUrl.js
+++ src/service/apiUrl.js
@@ -6,8 +6,8 @@ export default {
     // rootUrl: "http://localhost:8080/",
     // baseUrl: "http://localhost:8080",
 
-    rootUrl: "http://114.212.174.143:8088/",
-    baseUrl: "http://114.212.174.143:8088",
+    rootUrl: "http://114.212.184.2:8088/",
+    baseUrl: "http://114.212.184.2:8088",
 
     // rootUrl: "http://ahbfi.org.cn/",
     // baseUrl: "http://ahbfi.org.cn",
